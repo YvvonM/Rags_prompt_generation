3f209401b7f54fd73e75cf7b5712b8f144ce5b40
YvvonM <yvvonjemmymajala@gmail.com>
2024-01-21 17:45:20 +0300

    the development of rags

---
+++ extra.py
@@ -0,0 +1,3 @@
+llm_model_openai = "gpt-3.5-turbo" #affordable and very capable
+llm_model_cohere = "multilingual-22-12" #handles multilingual  search and embeddings
+PINECONE_INDEX_NAME = 'rag-cohere1' #pinecone environment
